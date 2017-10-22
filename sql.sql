732555908c494c249800f1424226bf8df600c7a8
ReginaSobral <31146624+ReginaSobral@users.noreply.github.com>
2017-10-22 20:34:01 -0200

    criada div de texto, alterado itens de usuario e banco

--- sql.sql
+++ sql.sql
@@ -1,14 +1,14 @@
 create table usuario (
 id serial primary key,
 nome char(20),
-email char(20),
+email char(60),
 senha char(10)
 );
 
 create table conteudo (
 id serial not null,
 id_usuario int,
-titulo char(20),
+titulo char(30),
 descricao char(60),
 midia char(60),
 foreign key (id_usuario) references usuario (id)
